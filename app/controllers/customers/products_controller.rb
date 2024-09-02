6d01f44dd744d826d2afc5a515bc94d6c9993fbc
joseph-digiteer <jbeni.dev@gmail.com>
2024-09-02 21:25:42 +0800

    add feature separating 2 different roles/users

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -1,6 +1,8 @@
 class Customers::ProductsController < ApplicationController
   include Pagy::Backend
 
+  #before_action :authorize_customer
+
   def index
     @q = Product.ransack(params[:q])
     @pagy, @products = pagy(@q.result.includes(:product_variants).where(active: true), items: 15)
@@ -10,4 +12,10 @@ class Customers::ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @variants = @product.product_variants.where(active: true)
   end
+
+  private
+
+  #def authorize_customer
+    #redirect_to(root_path, alert: 'Not authorized') unless current_user.customer?
+  #end
 end
